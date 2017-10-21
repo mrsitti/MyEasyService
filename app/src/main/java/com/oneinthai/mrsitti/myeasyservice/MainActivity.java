0d4cc921be91a31cd3eaf37e5d99d1c7ef824657
mrsitti <mrsitti@gmail.com>
2017-10-21 15:42:19 +0700

    Design UI  Simple v2 update 3

--- app/src/main/java/com/oneinthai/mrsitti/myeasyservice/MainActivity.java
+++ app/src/main/java/com/oneinthai/mrsitti/myeasyservice/MainActivity.java
@@ -3,11 +3,20 @@ package com.oneinthai.mrsitti.myeasyservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.oneinthai.mrsitti.myeasyservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain,new MainFragment()).commit();
+        }
+
+
     }
 }
