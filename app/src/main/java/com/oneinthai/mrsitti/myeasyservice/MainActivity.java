a77aa8cda10b080b0198cf7553ff92b654fac81f
mrsitti <mrsitti@gmail.com>
2017-10-22 15:39:21 +0700

    Select and Change Img.

--- app/src/main/java/com/oneinthai/mrsitti/myeasyservice/MainActivity.java
+++ app/src/main/java/com/oneinthai/mrsitti/myeasyservice/MainActivity.java
@@ -1,16 +1,29 @@
 package com.oneinthai.mrsitti.myeasyservice;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.oneinthai.mrsitti.myeasyservice.fragment.MainFragment;
+import com.oneinthai.mrsitti.myeasyservice.fragment.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +45,46 @@ public class MainActivity extends AppCompatActivity {
         //Text Controller
         textController();
 
+        //Crate Toolbar
+        crateToolbar();
+
+    } //Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState,
+                             @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void crateToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
@@ -45,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
         mainTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new MainFragment())
+                        .commit();
+
                 //Close Drawer ปิดเมนู
                 drawerLayout.closeDrawers();
 
@@ -56,6 +113,9 @@ public class MainActivity extends AppCompatActivity {
         secondTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.btnGoToSecound, new SecondFragment())
+                        .commit();
                 drawerLayout.closeDrawers();
             }
         });
@@ -65,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
         exitTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
                 drawerLayout.closeDrawers();
             }
         });
--- app/src/main/java/com/oneinthai/mrsitti/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/com/oneinthai/mrsitti/myeasyservice/fragment/SecondFragment.java
@@ -1,5 +1,9 @@
 package com.oneinthai.mrsitti.myeasyservice.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.oneinthai.mrsitti.myeasyservice.R;
 
@@ -16,6 +22,9 @@ import com.oneinthai.mrsitti.myeasyservice.R;
 
 public class SecondFragment extends Fragment{
     //กด Ctrl+o เลือก onActivityCreate กด Enter
+
+    private ImageView imageView;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -23,7 +32,50 @@ public class SecondFragment extends Fragment{
         //Back Controller กดย้อนกลับ ** กด Ctrl+Alt+m ถ้ามีหมายเหตุมากกว่า 1 วรรค จะต้องตั้งชื่อเอง
         backController();
 
+        //Image Controller
+        imageController();
+
     }   //Main Method
+
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_OK) {
+
+            //เลือกรูปแล้ว
+            try {
+
+                Uri uri=data.getData();
+                Bitmap bitmap= BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            //ออกโดยไม่เลือกรูป
+            Toast.makeText(getActivity(),"Not choose img.",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void imageController() {
+        imageView = getView().findViewById(R.id.imvHero);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,
+                        "Please. choose App"),3);
+            }
+        });
+
+
+    }
 
     private void backController() {
         Button button = getView().findViewById(R.id.btnBack);
